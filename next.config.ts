bb1cd8bb0e3bfcf8f524e1e3b04fe0b27a0d3064
kefah-rajha <kefah.rajha.1991@gmail.com>
2025-08-20 21:46:51 +0300

    add env

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,20 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const isProduction = process.env.NODE_ENV === 'production';
+    const apiUrl = isProduction 
+      ? 'https://youtubesummarynodeproduction.onrender.com'
+      : 'http://localhost:5000';
+
     return [
-    
       {
         source: '/api/:path*',
-        destination: `http://localhost:5000/api/:path*`,
+        destination: `${apiUrl}/api/:path*`,
       },
-     
     ];
+  },
+  env: {
+    CUSTOM_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
